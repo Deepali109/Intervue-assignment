c8431213d786a56c68c6ebe376153201a8a19bfa
Deepali109 <deepalisahu965@gmail.com>
2025-09-25 22:08:17 +0530

    okk

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
   cors: { origin: "*" },
 });
 
-// Serve frontend build
+// Serve frontend
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get(/.*/, (req, res) => {
